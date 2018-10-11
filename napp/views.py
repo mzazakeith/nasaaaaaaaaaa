64a80eb244d8375945288c1fc024ccda00d5e0b8
mzazakeith <mzazakeith@gmail.com>
2018-10-11 16:19:53 +0800

    managed to print json data from the api on the terminal

--- napp/views.py
+++ napp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
+import requests
+from django.http import JsonResponse
+from nasa import settings
+
 
 def index(request):
-
+    endpoint = 'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+    url = endpoint.format(api_key=settings.NASA_KEY)
+    response = requests.get(url)
+    results = response.json()
+    print(results)
     return render(request, 'index.html')
+
